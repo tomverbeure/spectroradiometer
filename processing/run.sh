805774362e42508f56b3ee070d283f455e642730
Tom Verbeure <hombre+github@gmail.com>
2019-02-11 20:31:19 -0800

    Add .json files

--- processing/run.sh
+++ processing/run.sh
@@ -1,3 +1,6 @@
 
-./spectro.rb ../measurements/x220_ips/x220_ips.json
+#./spectro.rb ../measurements/x220_ips/x220_ips.json
+#./spectro.rb ../measurements/iPhone\ 7s/iPhone_7s.json
+#./spectro.rb ../measurements/Monoprice\ 4K/monoprice_4k.json
+./spectro.rb ../measurements/Hannstar/hannstar.json
 
